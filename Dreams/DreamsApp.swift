47219e4688be056d5efc6e754d960a0fedc122c1
flexi03 <felixkircher.FK@icloud.com>
2025-04-04 23:14:57 +0200

    Initial Commit

---
+++ Dreams/DreamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamsApp.swift
+//  Dreams
+//
+//  Created by Felix Kircher on 04.04.25.
+//
+
+import SwiftUI
+
+@main
+struct DreamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
